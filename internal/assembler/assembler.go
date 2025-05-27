9b2385a192446f52e85da5044c2ffb46648de565
Ryan Ratajczak <ryantratajczak@gmail.com>
2025-05-26 19:01:56 -0600

    vastly simplify processing

--- internal/assembler/assembler.go
+++ internal/assembler/assembler.go
@@ -1,10 +1,7 @@
 package assembler
 
 import (
-	"bufio"
-	"cscasm/internal/lexer"
 	"cscasm/internal/parser"
-	"cscasm/internal/token"
 	"fmt"
 	"os"
 
@@ -19,42 +16,7 @@ func AssembleFile(filename string) ([]uint32, error) {
 	}
 	defer file.Close()
 
-	lex := lexer.NewLexer()
-	tokenizedLines := [][]*token.Token{}
-	symbolTable := make(map[string]int)
+	par := parser.NewParser(file)
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		tokens, _ := lex.TokenizeLine(line)
-
-		if len(tokens) == 0 {
-			continue
-		}
-
-		if tokens[0].Type == token.LABEL {
-			name := tokens[0].Literal.(string)
-			symbolTable[name] = len(tokenizedLines) + 1
-			continue
-		}
-
-		tokenizedLines = append(tokenizedLines, tokens)
-	}
-
-	par := parser.NewParser(symbolTable)
-	bytes := []uint32{}
-
-	for i, toks := range tokenizedLines {
-		ins, err := par.ParseInstructionFromTokens(toks)
-		if err != nil {
-			fmt.Printf("%d %0x\n", i+1, 0)
-			continue
-		}
-
-		fmt.Printf("%d %0x\n", i+1, ins.Encoding())
-		bytes = append(bytes, ins.Encoding())
-	}
-
-	return bytes, nil
+	return par.Parse()
 }
