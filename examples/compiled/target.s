df6dfea66b52350997f4916f6730f61300820653
Ryan Ratajczak <ryantratajczak@gmail.com>
2025-06-04 00:51:11 -0600

    clean up target.s

--- examples/compiled/target.s
+++ examples/compiled/target.s
@@ -1,5 +1,7 @@
-    movw    sp, #0x0000             ; initialize stack pointer to 0
-    movt    sp, #0x0000
+	movw    sp, #0x0000
+	movt    sp, #0x0000
+
+_start:
     movw    r4, #0x0000
     movt    r4, #0x3f20
     add     r2, r4, #0x08
@@ -8,32 +10,42 @@
     str     r3, [r2]
     movw    r2, #0x0000
     movt    r2, #0x0020
+    movw    r6, #7
+    str     r6, [sp], #4
 
 loop:
-    add     r3, r4, #0x1c
-    str     r2, [r3]
-    stmea   sp!, {r0-r12}           ; push registers 0 - 12 onto the stack
-    movw    r12, #0x0900            ; move 4 million into r12
-    movt    r12, #0x3d
-    str     r12, [sp], #4           ; push the value at r12 onto the stack
+    bl      blink
+    movw    r5, #0x4240
+    movt    r5, #0xf
+    str     r5, [sp], #4
     bl      delay
-    ldmea   sp!, {r0-r12}           ; restore registers 0 - 12 from the stack
+    subs    r6, r6, #1
+    bne     loop
 
-    add     r3, r4, #0x28
-    str     r2, [r3]
-    stmea   sp!, {r0-r12}
-    movw    r12, #0x4240            ; move 1 million into r12 this time
-    movt    r12, #0xf
-    str     r12, [sp], #4
+    movw    r5, #0x0900
+    movt    r5, #0x3d
+    str     r5, [sp], #4
     bl      delay
-    ldmea   sp!, {r0-r12}
 
+    ldr     r6, [sp, #-4]
     b       loop
 
+blink:
+    str     lr, [sp], #4
+    add     r3, r4, #0x1c
+    str     r2, [r3]
+    movw    r5, #0x4240
+    movt    r5, #0xf
+    str     r5, [sp], #4
+    bl      delay
+    add     r3, r4, #0x28
+    str     r2, [r3]
+    ldr     lr, [sp, #-4]!
+    bx      lr
+
 delay:
-    ldr     r5, [sp, #-4]!          ; load the top value from stack into r5
+    ldr     r5, [sp, #-4]!
 delay_loop:
     subs    r5, r5, #1
     bge     delay_loop
     bx      lr
-
